d6a1e021baf145ff4aa8a82fb5f4075d2c2cdbea
BorislavAnchev <bobianchev@outlook.com>
2020-06-17 13:48:01 +0300

    add APM-NgRx project to the portfolio

---
+++ APM-NgRx/src/app/home/shell.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'pm-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ShellComponent {
+}
